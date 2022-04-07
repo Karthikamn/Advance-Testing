4462ed9e949bb6d216842a303e2c40aae772cbfc
skarthika <skarthika@DS-PG020YBA.HCLT.CORP.HCL.IN>
2022-04-07 15:59:51 +0530

    commit

--- RestAPI/src/test/java/sample/APITesting.java
+++ RestAPI/src/test/java/sample/APITesting.java
@@ -1,46 +1,11 @@
 package sample;
 
-import static org.testng.Assert.assertEquals;
 
-import org.json.simple.JSONObject;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.*;
-
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
-import static org.hamcrest.Matchers.*;
 
 public class APITesting {
 	
-	@BeforeClass
-	public void bf() {
-		RestAssured.useRelaxedHTTPSValidation();
+	public static void main(String[] args) {
+		System.out.println("hello");
 	}
-	
-	
-	@Test
-	public void get_method() {
-		
-		/*JSONObject r1=new JSONObject();
-		
-		r1.put("name", "morpheus");
-		r1.put("job", "singer");
-		
-		System.out.println(r1.toJSONString());
-		*/
-		given().
-		header("Content-Type","application/json").
-		delete("https://reqres.in/api/users/2").
-		then().statusCode(204);
-		
-		
-	}
-
-
-	
 
 }
